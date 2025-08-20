7f19fc805736c3afc7b517abc624c288326380de
Dimas Fajar <153111942+fajardison@users.noreply.github.com>
2025-08-20 13:45:57 +0700

    Create getTargetName.js

---
+++ helpers/getTargetName.js
@@ -0,0 +1,11 @@
+/**
+* Get a descriptive name for the target
+* @param {any} target
+* @returns {string}
+ */
+export default function getTargetName(target) {
+  if (target === null) return 'null';
+  const t = typeof target;
+  if (t !== 'object' && t !== 'function') return t;
+  return target.constructor?.name || 'Object';
+}
